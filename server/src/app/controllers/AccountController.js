b513e09ede761e761428490e3eda3a25270210c2
daoh329 <daoh329@gmail.com>
2023-08-11 11:14:29 +0700

    code controller SignIn

--- server/src/app/controllers/AccountController.js
+++ server/src/app/controllers/AccountController.js
@@ -2,11 +2,25 @@ const mysql2 = require('../../config/db/mySQLConnection');
 
 class AccountController {
     async signup(req, res, next) {
-        
+    
     }
 
-    async signin() {
+    async signin(identifier, password) {
+    
 
+    const query = 'SELECT * FROM users WHERE (username = ? OR email = ?) AND password = ?';
+    db.query(query, [identifier, identifier, password], (err, results) => {
+        if (err) {
+            return("Lỗi truy vấn cơ sở dữ liệu");
+        }
+
+        if (results.length > 0) {
+            // thành công trả về dữ liệu người dùng.
+            return(results);            
+        } else {
+            return("sai thông tin đăng nhập");            
+        }
+    });
     }
 }
 
