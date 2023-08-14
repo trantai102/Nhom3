e9dbc8a6893d5bc5faf642f2f4acaab1bd2ea3f3
trantai102 <100552532+trantai102@users.noreply.github.com>
2023-08-14 09:44:39 +0700

    form trang chu

--- client/src/components/SignInForm.jsx
+++ client/src/components/SignInForm.jsx
@@ -1,30 +1,35 @@
 // src/components/SignInForm.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import SignupForm from './SignupForm';
 
-const SignInForm = () => {
+const SignInForm = ({onLogin}) => {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [showSignUpForm, setShowSignUpForm] = useState(false); 
 
   const [User, setUser] = useState([]);//thông tin user được trả về tại đây
 
   const handleSignIn = () => {
-    axios.post('http://localhost:3001/account/signin',{
-      name: name,
-      password:password    
-    }
-    ).then(response => {
-      setUser({...response.data});
-      // Xử lý dữ liệu từ API response
-      alert("Đăng nhập thành công.")
-    })
-    .catch(error => {
-      console.error('Error:', error);     
-      // Xử lý lỗi
-    });
-    
+    // axios.post('http://localhost:3001/account/signin',{
+    //   name: name,
+    //   password:password    
+    // }
+    // ).then(response => {
+    //   setUser({...response.data});
+    //   // Xử lý dữ liệu từ API response
+    //   alert("Đăng nhập thành công.")
+    //   onLogin();
+    // })
+    // .catch(error => {
+    //   console.error('Error:', error);     
+    //   // Xử lý lỗi
+    // });
+    onLogin();
   };
-
+  const toggleSignUpForm = () => {
+    setShowSignUpForm(!showSignUpForm);
+  };
   return (
     <div className="signin-form">
       <h2>Đăng nhập</h2>
@@ -37,6 +42,9 @@ const SignInForm = () => {
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
       <button onClick={handleSignIn}>Đăng nhập</button>
+      <button onClick={toggleSignUpForm}>Đăng ký</button>
+      {/* Show SignUpForm if showSignUpForm is true */}
+      {showSignUpForm && <SignupForm />}
     </div>
   );
 };
